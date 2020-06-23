d305ed9bf95676c305e234c19f0b3b79fcbccb70
854731306@qq.com <854731306@qq.com>
2020-06-23 18:26:01 +0800

    初始

--- src/main/java/com/shanmu2020/jdk11/collection/CollectionTest.java
+++ src/main/java/com/shanmu2020/jdk11/collection/CollectionTest.java
@@ -1,4 +1,18 @@
 package com.shanmu2020.jdk11.collection;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionTest {
+    @Test
+    public void test5() {
+        List<String> list = List.of("Java", "Python", "C"); //不可变集合
+        List<String> copy = List.copyOf(list); //copyOf判断是否是不可变集合类型，如果是直接返回
+        System.out.println(list == copy); // true
+        List<String> list1 = new ArrayList<String>(); // 这里返回正常的集合
+        List<String> copy1 = List.copyOf(list1); // 这里返回一个不可变集合
+        System.out.println(list1 == copy1); // false
+    }
 }
--- src/main/java/com/shanmu2020/jdk11/string/StringTest.java
+++ src/main/java/com/shanmu2020/jdk11/string/StringTest.java
@@ -5,23 +5,22 @@ import org.junit.Test;
 public class StringTest {
     @Test
     public void test5() {
-        // 判断字符串是否为空白
         boolean blank = " ".isBlank();// true
-        System.out.println("blank = " + blank);
-        // 去除首尾空格
+        System.out.println("判断字符串是否为空白 " + blank);
+
         String strip = " Hello Java11 ".strip();// "Hello Java11"
-        System.out.println("strip = " + strip);
-        // 去除尾部空格
+        System.out.println("去除首尾空格 " + strip);
+
         String stripTrailing = " Hello Java11 ".stripTrailing();// " Hello Java11"
-        System.out.println("stripTrailing = " + stripTrailing);
-        // 去除首部空格
+        System.out.println("去除尾部空格 " + stripTrailing);
+
         String stripLeading = " Hello Java11 ".stripLeading();// "Hello Java11 "
-        System.out.println("stripLeading = " + stripLeading);
-        // 复制字符串
+        System.out.println("去除首部空格 " + stripLeading);
+
         String repeat = "Java11".repeat(3);// "Java11Java11Java11"
-        System.out.println("repeat = " + repeat);
-        // 行数统计
+        System.out.println("复制字符串 " + repeat);
+
         long count = "A\nB\nC".lines().count();// 3
-        System.out.println("count = " + count);
+        System.out.println("行数统计 " + count);
     }
 }
